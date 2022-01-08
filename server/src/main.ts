6b98290f251179e02c5ef045802a81da1b86ae48
Yashas Hebbarabailu <yashas.hebbarbail@gmail.com>
2022-01-08 15:38:04 +0530

    renaming env variable

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'));
-  await app.listen(process.env.port || 80);
+  await app.listen(process.env.PORT || 80);
 }
 bootstrap();
